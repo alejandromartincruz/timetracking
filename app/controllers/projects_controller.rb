8d9ff7ae93097a60d217565bcb8aa7f8e6696784
alejandromartincruz <socorreitor@gmail.com>
2015-11-03 10:05:55 +0100

    Added a form to create projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,4 +11,17 @@ class ProjectsController < ApplicationController
 		
 		render :show
 	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		Project.create({
+			title:params[:project][:title],
+			description:params[:project][:description],
+			priority:params[:project][:priority]
+			})
+		redirect_to "/projects"
+	end
 end

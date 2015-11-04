b00d5c35ea46c73d8bdad103ec4dd6e6e4e1e022
alejandromartincruz <socorreitor@gmail.com>
2015-11-04 12:38:00 +0100

    Add create new entries through form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,4 +7,22 @@ class EntriesController < ApplicationController
 			@entries = @project.entries
 		end
 	end
+
+	def new
+		@project = Project.find_by(id: params[:project_id])
+		@entry = Entry.new
+	end
+
+	def create
+		project_id = params[:project_id]
+		project = Project.find_by(id: project_id)
+		entry = project.entries.new({
+			hours: params[:entry][:hours],
+			minutes: params[:entry][:minutes],
+			data: params[:entry][:data],
+			comments: params[:entry][:comments]
+			})
+		entry.save
+		redirect_to "/projects/#{project_id}/entries"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get "/projects"=> "projects#index"
   post "/projects"=> "projects#create"
   get "/projects/new" => "projects#new"
+  get "/projects/:project_id/entries/new"=> "entries#new"
   get "/projects/:project_id/entries"=> "entries#index"
   get "/projects/:project_id"=> "projects#show"
+  post "/projects/:project_id/entries" => "entries#create", as: "project_entries"
   
 end
